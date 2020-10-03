fb1a1625cf2410fdee5bb732bbc8f2b354da4ede
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2020-10-03 20:36:57 +0000

    autosave

--- docs/source/.ipynb_checkpoints/conf-checkpoint.py
+++ docs/source/.ipynb_checkpoints/conf-checkpoint.py
@@ -79,7 +79,7 @@ latex_elements = {
 \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
 \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
 \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
-\begin{CJK}{UTF8}{gbsn}
+\begin{CJK*}{UTF8}{bmsi}
 \AtEndDocument{\end{CJK}}
 ''',
 }
